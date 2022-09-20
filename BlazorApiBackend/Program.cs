77f7db4a7ab0000f95f79946936962a0908249b5
Rick Tauss <tauss.rick@gmail.com>
2022-09-20 16:27:29 +0200

    Added DataBinding and Component examples

--- BlazorApiBackend/Program.cs
+++ BlazorApiBackend/Program.cs
@@ -24,11 +24,11 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
